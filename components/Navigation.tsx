3c9fa9b0f81c8940e0538e53eb1cbc0212b069df
Mario Milosevic <128989997+MarioMilosevic@users.noreply.github.com>
2025-06-03 19:49:39 +0200

    Add account icon in navigation

--- components/Navigation.tsx
+++ components/Navigation.tsx
@@ -1,25 +1,29 @@
-import {
-  NavigationMenu,
-  NavigationMenuItem,
-  NavigationMenuList,
-} from "@/components/ui/navigation-menu";
+import { NavigationMenu } from "@/components/ui/navigation-menu";
 import FloatingLabelInput from "@/components/ui/FloatingLabelInput";
 import { Button } from "@/components/ui/button";
-import logo from "../public/logo/logo.avif";
+import { RiAccountCircleLine } from "react-icons/ri";
+import Link from "next/link";
+import logo from "../public/logo/ebay.jpg";
 import Image from "next/image";
 
 export default function Navigation() {
   return (
-    <NavigationMenu className="border-b-neutral-200 border-b max-w-full">
-      <Image src={logo} alt="logo" />
-      <FloatingLabelInput className="w-full">Search</FloatingLabelInput>
-      <Button variant={"outline"}>Search</Button>
-      {/* <NavigationMenuList className="w-1/2 bg-red-500">
-        <NavigationMenuItem className="flex gap-2 w-full">
-          <FloatingLabelInput className="w-full" />
-          <Button variant={"outline"}>Search</Button>
-        </NavigationMenuItem>
-      </NavigationMenuList> */}
+    <NavigationMenu className="border-b-neutral-200 border-b max-w-full h-[100px] gap-8">
+      <Link href={"/"}>
+        <Image
+          src={logo}
+          alt="logo"
+          width={150}
+          className="h-full object-cover cursor-pointer"
+        />
+      </Link>
+      <FloatingLabelInput className="w-2/3">
+        Search for anything
+      </FloatingLabelInput>
+      <div className="flex gap-4 items-center">
+        <Button variant={"outline"}>Search</Button>
+        <RiAccountCircleLine size={30} className="cursor-pointer" />
+      </div>
     </NavigationMenu>
   );
 }

124f80282f9549aa03efa5ca83bf464193a1028e
Mario Milosevic <128989997+MarioMilosevic@users.noreply.github.com>
2025-06-12 13:16:35 +0200

    Check if user already exists by comparing email

--- actions/sign-up.ts
+++ actions/sign-up.ts
@@ -2,10 +2,12 @@
 
 import { z } from "zod";
 import { signUpFormSchema } from "@/lib/zodSchemas";
+import prisma from "@/prisma/prismaClient";
+import { NextResponse } from "next/server";
 
 export async function signUpUser(values: z.infer<typeof signUpFormSchema>) {
   const result = signUpFormSchema.safeParse(values);
-  console.log('ovo je result', result)
+  console.log("ovo je result", result);
 
   if (!result.success) {
     return {
@@ -13,9 +15,21 @@ export async function signUpUser(values: z.infer<typeof signUpFormSchema>) {
       message: "Something went wrong",
     };
   }
+  const { email, firstName, lastName } = result.data;
 
+  const existingEmail = await prisma.user.findUnique({
+    where: { email },
+  });
 
-  
+  console.log("ovo je existing email", existingEmail);
+
+  if (existingEmail) {
+    return {
+      status: "failure",
+      message: "User with this email already exists",
+    };
+  }
+
   return {
     status: "success",
     message: "Request sent successfully",

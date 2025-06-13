e149069ed36fc7df4e7ebe96e7f538805fcc2027
Mario Milosevic <128989997+MarioMilosevic@users.noreply.github.com>
2025-06-13 16:16:09 +0200

    Add middleware to redirect if user is signed in from public routes to home route

---
+++ middleware.ts
@@ -0,0 +1,24 @@
+import { NextRequest, NextResponse } from "next/server";
+import { decrypt } from "./lib/session";
+
+const protectedRoutes = ["/dashboard"];
+const publicRoutes = ["/login", "/sign-up"];
+
+export default async function middleware(req: NextRequest) {
+  const path = req.nextUrl.pathname;
+  const isProtectedRoute = protectedRoutes.includes(path);
+  const isPublicRoute = publicRoutes.includes(path);
+
+  const cookie = req.cookies.get("session")?.value;
+  const session = await decrypt(cookie);
+
+  if (isProtectedRoute && !session?.userId) {
+    return NextResponse.redirect(new URL("/login", req.url));
+  }
+
+  if (isPublicRoute && session?.userId) {
+    return NextResponse.redirect(new URL("/", req.url));
+  }
+
+  return NextResponse.next();
+}

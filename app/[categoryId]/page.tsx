988971f237cc59799f7a1446a31b78985e609243
Mario Milosevic <128989997+MarioMilosevic@users.noreply.github.com>
2025-06-17 09:55:43 +0200

    Add not found page

--- app/[categoryId]/page.tsx
+++ app/[categoryId]/page.tsx
@@ -1,12 +1,18 @@
 import { CategoriesSidebar } from "@/components/categories/CategoriesSidebar";
 import { SidebarProvider } from "@/components/ui/sidebar";
+import { categories } from "@/lib/constants";
+import { notFound } from "next/navigation";
 export default async function page({
   params,
 }: {
   params: Promise<{ categoryId: string }>;
 }) {
   const { categoryId } = await params;
-console.log(categoryId)
+  console.log(categoryId);
+  console.log(categories);
+  if (categories.findIndex((category) => category.id === categoryId)) {
+    notFound();
+  }
   return (
     <>
       <SidebarProvider>

94df3154ea4597da9a9d783a5f956e83d2128f59
Mario Milosevic <128989997+MarioMilosevic@users.noreply.github.com>
2025-06-19 13:08:25 +0200

    Write additional models for database

--- middleware.ts
+++ middleware.ts
@@ -1,7 +1,7 @@
 import { NextRequest, NextResponse } from "next/server";
 import { decrypt } from "./lib/session";
 
-const protectedRoutes = ["/dashboard"];
+const protectedRoutes = ["/dashboard", "/add"];
 const publicRoutes = ["/login", "/sign-up"];
 
 export default async function middleware(req: NextRequest) {
